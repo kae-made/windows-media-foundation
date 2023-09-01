2048e4c809cc91ea83b1b0d34916a55358e97bbf
kae-made <93808496+kae-made@users.noreply.github.com>
2023-09-01 15:22:21 +0900

    Add wav file dump tool

---
+++ Tools/WpfAppDumpAndWav/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDumpAndWav
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        List<byte[]> contents = new List<byte[]>();
+
+        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                tbFileName.Text = dialog.FileName;
+
+                using (var fs = File.OpenRead(tbFileName.Text))
+                {
+                    fs.Seek(0, SeekOrigin.End);
+                    long fileSize = fs.Position;
+                    fs.Seek(0, SeekOrigin.Begin);
+                    int readChunk = 16;
+                    var buffer = new byte[readChunk];
+                    int currentSize = 0;
+                    contents.Clear();
+                    while (currentSize < fileSize)
+                    {
+                        int readSize = fs.Read(buffer, 0, readChunk);
+                        var content = new byte[readSize];
+                        Array.Copy(buffer, 0, content, 0, readSize);
+                        contents.Add(content);
+                        currentSize += readSize;
+                    }
+
+                    var sb = new StringBuilder();
+                    int lineIndex = 0;
+                    foreach (var line in contents)
+                    {
+                        string asscii = "";
+                        sb.Append(lineIndex.ToString("x8"));
+                        int index = 0;
+                        foreach (byte b in line)
+                        {
+                            if ((index % 4) == 0)
+                            {
+                                sb.Append(" ");
+                            }
+                            sb.Append(b.ToString("x2"));
+                            if (0x20 <= b && b <= 0x7e)
+                            {
+                                asscii += (char)b;
+                            }
+                            else
+                            {
+                                asscii += ".";
+                            }
+                            index++;
+                        }
+                        sb.AppendLine($" {asscii}");
+                        lineIndex += readChunk;
+                    }
+                    tbContent.Text = sb.ToString();
+
+                    fs.Seek(0, SeekOrigin.Begin);
+                    soundData = new PCMSoundData();
+                    try
+                    {
+                        soundData.Load(fs);
+
+                        ShowRIFFContent(soundData);
+                        buttonExport.IsEnabled = true;
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private PCMSoundData soundData;
+
+        private void ShowRIFFContent(PCMSoundData soundData)
+        {
+            tbRiffId.Text = "RIFF";
+            tbRiffSize.Text = $"{soundData.RiffChunkSize}";
+            tbFormat.Text = soundData.Format;
+            tbJunkSize.Text = $"{soundData.JunkBytes}";
+            tbFmt.Text = "fmt";
+            tbSizeOfFmt.Text = $"{soundData.FmtChunkBytes}";
+            tbAudioFormat.Text = $"{soundData.AudioFormat}";
+            tbChannels.Text = $"{soundData.Channels}";
+            tbSamplingFreq.Text = $"{soundData.SamplingFrequency}";
+            tbBytesPerSec.Text = $"{soundData.BytesPerSecond}";
+            tbSizeOfBlock.Text = $"{soundData.BytesOfBlock}";
+            tbBitsOfSample.Text = $"{soundData.BitsPerSample}";
+            tbSubChunkIdentifier.Text = "data";
+            tbSizeOfSubChunk.Text = $"{soundData.SubChunkSize}";
+            tbNumOfRecords.Text = $"{soundData.NumOfRecord}";
+
+            string paramExt = "";
+            for(int i = 0; i < soundData.ExtParamsSize; i++)
+            {
+                paramExt += string.Format("x2", soundData.ExtParams[i]);
+            }
+            tbParamExt.Text = paramExt;
+        }
+
+        private void buttonExport_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "CVS Files|*.cvs";
+            if (dialog.ShowDialog() == true)
+            {
+                using (var stream = File.Create(dialog.FileName))
+                {
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        writer.WriteLine($"RIFF Size,{soundData.RiffChunkSize}");
+                        writer.WriteLine($"Format,{soundData.Format}");
+                        writer.WriteLine($"Junk Size,{soundData.JunkBytes}");
+                        writer.WriteLine($"fmt Chunk Bytes,{soundData.FmtChunkBytes}");
+                        writer.WriteLine($"Audio Format,{soundData.AudioFormat}");
+                        writer.WriteLine($"Channels,{soundData.Channels}");
+                        writer.WriteLine($"Sampling Frequency,{soundData.SamplingFrequency}");
+                        writer.WriteLine($"Bytes per Second,{soundData.BytesPerSecond}");
+                        writer.WriteLine($"Bytes of Block,{soundData.BytesOfBlock}");
+                        writer.WriteLine($"Bits of Sampling,{soundData.BitsPerSample}");
+                        writer.WriteLine($"Bytes of Sub Chunk,{soundData.SubChunkSize}");
+                        writer.WriteLine($"Number of Records,{soundData.NumOfRecord}");
+                        for(var r = 0; r < soundData.NumOfRecord; r++)
+                        {
+                            string dataLine = "";
+                            for (var c=0; c < soundData.Channels; c++)
+                            {
+                                if (!string.IsNullOrEmpty(dataLine))
+                                {
+                                    dataLine += ",";
+                                }
+                                dataLine += soundData.WaveData[c][r];
+                            }
+                            writer.WriteLine(dataLine);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tools/WpfAppDumpAndWav/PCMSoundData.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDumpAndWav
+{
+    public class PCMSoundData
+    {
+        public uint RiffChunkSize { get; set; }
+        public string Format { get; set; }
+        public bool HasJunk { get; set; }
+        public UInt32 JunkBytes { get; set; }
+        public UInt32 FmtChunkBytes { get; set; }
+        public UInt16 AudioFormat { get; set; }
+        public UInt16 Channels { get; set; }
+        public UInt32 SamplingFrequency { get; set; }
+        public UInt32 BytesPerSecond { get; set; }
+        public UInt16 BytesOfBlock { get; set; }
+        public UInt16 BitsPerSample { get; set; }
+        public UInt16 ExtParamsSize { get; set; }
+        public byte[] ExtParams { get; set; }
+        public UInt32 SubChunkSize { get; set; }
+        public UInt32 NumOfRecord { get; set; }
+        public List<List<int>> WaveData { get; set; }
+
+        public PCMSoundData()
+        {
+            WaveData = new List<List<int>>();
+        }
+
+        public void Load(Stream dataStream)
+        {
+            byte[] buf2 = new byte[2];
+            byte[] buf4 = new byte[4];
+
+            int readSize = dataStream.Read(buf4, 0, buf4.Length);
+            string data = System.Text.Encoding.UTF8.GetString(buf4);
+            if (data != "RIFF")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            if (readSize != buf4.Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            RiffChunkSize = BitConverter.ToUInt32(buf4);
+
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            data = System.Text.Encoding.UTF8.GetString(buf4);
+            if (data != "WAVE")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Format = data;
+
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            data = System.Text.Encoding.UTF8.GetString(buf4);
+            if (data == "fmt ")
+            {
+                //
+            }
+            else if (data == "JUNK")
+            {
+                HasJunk = true;
+                readSize = dataStream.Read(buf4, 0, buf4.Length);
+                JunkBytes = BitConverter.ToUInt32(buf4);
+                if (JunkBytes > 0)
+                {
+                    var tmpBuf = new byte[JunkBytes];
+                    readSize = dataStream.Read(tmpBuf, 0, tmpBuf.Length);
+                    if (readSize != tmpBuf.Length) { throw new ArgumentOutOfRangeException(); }
+                    if ((JunkBytes % 2) != 0)
+                    {
+                        readSize = dataStream.Read(buf2, 0, 1);
+                        if (readSize != 1)
+                        {
+                            throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                    readSize = dataStream.Read(buf4, 0, buf4.Length);
+                    data = System.Text.Encoding.UTF8.GetString(buf4);
+                    if (data != "fmt ")
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            FmtChunkBytes = BitConverter.ToUInt32(buf4);
+
+            readSize = dataStream.Read(buf2, 0, buf2.Length);
+            AudioFormat = BitConverter.ToUInt16(buf2);
+
+            readSize = dataStream.Read(buf2, 0, buf2.Length);
+            Channels = BitConverter.ToUInt16(buf2);
+
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            SamplingFrequency = BitConverter.ToUInt32(buf4);
+
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            BytesPerSecond = BitConverter.ToUInt32(buf4);
+
+            readSize = dataStream.Read(buf2, 0, buf2.Length);
+            BytesOfBlock = BitConverter.ToUInt16(buf2);
+
+            readSize = dataStream.Read(buf2, 0, buf2.Length);
+            BitsPerSample = BitConverter.ToUInt16(buf2);
+
+            if (FmtChunkBytes > 16)
+            {
+
+                readSize = dataStream.Read(buf2, 0, buf2.Length);
+                ExtParamsSize = BitConverter.ToUInt16(buf2);
+                if (ExtParamsSize > 0)
+                {
+                    ExtParams = new byte[ExtParamsSize];
+                    readSize = dataStream.Read(ExtParams, 0, ExtParamsSize);
+                }
+            }
+
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            data = System.Text.Encoding.UTF8.GetString(buf4);
+
+            if (data != "data")
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            readSize = dataStream.Read(buf4, 0, buf4.Length);
+            SubChunkSize = BitConverter.ToUInt32(buf4);
+
+            NumOfRecord = SubChunkSize / Channels;
+            int bytesOfSample = BitsPerSample / 8;
+            NumOfRecord /= (UInt32)bytesOfSample;
+
+            for (int i = 0; i < Channels; i++)
+            {
+                var dataUnits = new List<int>();
+                WaveData.Add(dataUnits);
+            }
+
+            int readBytePerLine = bytesOfSample * Channels;
+
+            for (int i = 0; i < NumOfRecord; i++)
+            {
+                for (int c = 0; c < Channels; c++)
+                {
+                    readSize = dataStream.Read(buf2, 0, bytesOfSample);
+                    int frag = 0;
+                    if (bytesOfSample == 1)
+                    {
+                        frag |= buf2[0];
+                    }
+                    else if (bytesOfSample == 2)
+                    {
+                        frag = BitConverter.ToInt16(buf2);
+                    }
+                    WaveData[c].Add(frag);
+                }
+            }
+        }
+    }
+}
